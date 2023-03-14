313c298f5983350680a4fa1bbe795e04e1207ee0
Stas Voievodin <vstasv@i.ua>
2023-03-14 10:25:41 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ulEl = document.querySelector("#ingredients");
+
+for (let i = 0; i < ingredients.length; i++) {
+    let liEl = document.createElement('li');
+    liEl.className = '.item';
+  liEl.textContent = ingredients[i];
+    ulEl.appendChild(liEl);
+};
+
+// console.log(ulEl);
+
+
+
+
