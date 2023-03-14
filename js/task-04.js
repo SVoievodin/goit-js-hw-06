4a1e7e0348dda588802723a08686836fa71debc2
Stas Voievodin <vstasv@i.ua>
2023-03-14 13:49:50 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+const valueEl = document.querySelector('#value');
+const incrEl = document.querySelector('[data-action="increment"]');
+const decrEl = document.querySelector('[data-action="decrement"]');
+
+const counter = {
+    value: 0,
+    incrFn() { this.value += 1 },
+    decrFn() { this.value -= 1 },
+}
+
+incrEl.addEventListener('click', () => {
+    counter.incrFn();
+    valueEl.textContent = counter.value;
+});
+decrEl.addEventListener('click', () => {
+    counter.decrFn();
+    valueEl.textContent = counter.value;
+});
+
