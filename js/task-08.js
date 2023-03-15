1807c131cf04cf554e7c5c32e82d98e9c24fa5a8
Stas Voievodin <vstasv@i.ua>
2023-03-15 19:39:58 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,40 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onLogin);
+console.log(form)
+
+function onLogin(event) {
+    event.preventDefault();  // не перезавантажує сторінку
+
+    const dataFromForm = {
+        email: event.currentTarget.elements.email.value,
+        password: event.currentTarget.elements.password.value,
+    }
+
+    if (dataFromForm.email === "" || dataFromForm.password === "") {
+        return alert("Будь ласка, заповніть всі поля форми");
+    }
+
+    console.log(dataFromForm);
+    event.currentTarget.reset();
+}
+
+
+//* const formData = new FormData(event.currentTarget); //збір для відправки даних на сервер (об’єкт з всіма заповненими інпутами (назва: значення))
+
+//!Виконано без elements
+// function onLogin(event) {
+//     event.preventDefault();  // не перезавантажує сторінку
+//     const {
+//         elements: { email, password }
+//     } = event.currentTarget;
+
+//     if (email.value === "" || password.value === "") {
+//         return alert("Будь ласка, заповніть всі поля форми");
+//     }
+
+//     console.log(`Login: ${email.value}, Password: ${password.value}`);
+//     event.currentTarget.reset();
+
+// }
