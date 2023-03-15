75c48d9e096ad9c1b4b4206010301dc3b9a916ca
Stas Voievodin <vstasv@i.ua>
2023-03-15 16:25:50 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,25 @@ const inputRef = document.querySelector('#validation-input');
 // console.log(inputRef.dataset.length);
 
 function validate(event) {
-    if (event.currentTarget.value.length < inputRef.dataset.length)
-        return inputRef.classList.remove('invalid') || inputRef.classList.add('valid');
-    inputRef.classList.replace('valid', 'invalid');
+    if (event.currentTarget.value.length != inputRef.dataset.length)
+        return inputRef.classList.add('invalid');
+    inputRef.classList.replace('invalid', 'valid');
     // console.log(event.currentTarget.value.length)
 }
 
 inputRef.addEventListener('blur', validate);
+
+
+
+
+
+
+
+
+// ______Варіант з "не більше 6"____
+// function validate(event) {
+//     if (event.currentTarget.value.length < inputRef.dataset.length)
+//         return inputRef.classList.remove('invalid') || inputRef.classList.add('valid');
+//     inputRef.classList.replace('valid', 'invalid');
+//     // console.log(event.currentTarget.value.length)
+// }
