8354d5dd0453a1123677883b2c9b1c4b376cd32e
Stas Voievodin <vstasv@i.ua>
2023-03-16 11:57:09 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const btnGen = document.querySelector('button.change-color');
+const txtGen = document.querySelector('span.color');
+const body = document.querySelector('body');
+
+btnGen.addEventListener('click', colorationFn)
+
+function colorationFn() {
+  txtGen.textContent = `${getRandomHexColor()}`;
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
